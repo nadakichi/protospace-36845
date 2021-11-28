cb236913d067fc379ab19b4abae0ec0fb29030f9
nadakichi <shogo.oilt@gmail.com>
2021-11-28 20:07:35 +0900

    Update prototypes_controller.rb

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
-  before_action :set_prototype, only: [:show, :edit, :destroy, :update]
-  before_action :move_to_index, except: [:show, :index]
+  before_action :set_prototype, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @prototypes = Prototype.all
